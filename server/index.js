839cf7cd6614ee20569b329595c38675417fec13
azikreed <azikreed@gmail.com>
2022-02-21 01:02:43 +0500

    fixed

--- server/index.js
+++ server/index.js
@@ -1,27 +1,22 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
-const models = require('./models/models');
-const cors = require('cors');
+const models = require("./models/models");
+const cors = require("cors");
 //set PORT
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
-app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.status(200).json({message: "WORKING!!!"})
-})
+app.use(express.json());
 
 const start = async () => {
   try {
-    await sequelize.authenticate()
-    await sequelize.sync()
+    await sequelize.authenticate();
+    await sequelize.sync();
     app.listen(PORT, () => console.log(`Server has been started on ${PORT}`));
   } catch (err) {
     console.log(err);
   }
 };
 
-start()
+start();
