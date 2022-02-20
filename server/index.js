6cf6804546af18bd79ddafdc88016052e0619491
azikreed <azikreed@gmail.com>
2022-02-21 00:58:49 +0500

    completed database setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require("express");
-const app = express();
 const sequelize = require("./db");
+const models = require('./models/models');
+const cors = require('cors');
 //set PORT
 const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "WORKING!!!"})
+})
 
 const start = async () => {
   try {
@@ -13,3 +23,5 @@ const start = async () => {
     console.log(err);
   }
 };
+
+start()
